d3944bf90bb24db0898eca493f52b0978e38e39d
fellayoucef <youceff.fella@gmail.com>
2018-10-28 23:30:47 +0100

    formulaire
    bonjour monsieur pouvez vous m'aidez a cacher les deux bandes de message qui doivent s'afficher uniquement pour les condition  du JS

---
+++ tp2/formulaire.js
@@ -0,0 +1,18 @@
+function validation() {
+    var minlength=5;
+    var nom = document.getElementById("nom");
+    var prenom = document.getElementById("prenom");
+    var dn = document.getElementById("dn");
+    var adresse = document.getElementById("adresse");
+    var mail = document.getElementById("mail");
+
+
+    if( nom.value.length >= minlength && prenom.value.length >= minlength 
+        && dn.value.length >= minlength && adresse.value.length >= minlength && mail.value.length >= minlength){
+
+        document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#prenom").value;
+    }
+    else{
+        document.getElementById("error").innerHTML = "Les champs doivent avoir minimum "+minlength+" caractéres";
+    }
+    }
