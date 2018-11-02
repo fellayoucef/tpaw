bc9dd0d32f7b8f03f76bde592115c344d00e5cd3
fellayoucef <youceff.fella@gmail.com>
2018-11-02 23:51:39 +0100

    div error et resultat
    cacher les div initialement et les afficher q'une fois la validation faite

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -9,10 +9,11 @@ function validation() {
 
     if( nom.value.length >= minlength && prenom.value.length >= minlength 
         && dn.value.length >= minlength && adresse.value.length >= minlength && mail.value.length >= minlength){
-
+            document.getElementById('resultat').style.display='block';
         document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#prenom").value;
     }
     else{
+        document.getElementById('error').style.display='block';
         document.getElementById("error").innerHTML = "Les champs doivent avoir minimum "+minlength+" caractéres";
     }
     }
