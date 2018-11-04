d3b9c35b959e39a36e8113b4b2e7fd8374956926
fellayoucef <youceff.fella@gmail.com>
2018-11-04 21:02:57 +0100

    TP2final
    TP3 probleme avec affichage de la modal (elle disparait) et reste a mettre en place le datepicker

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -1,54 +1,63 @@
-$( document).ready(function() {
+$( document ).ready(function() {
     // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
     // voir plus : https://www.w3schools.com/js/js_htmldom.asp
      console.log( "DOM ready!" );
+    
+            $(document).keyup(function(){
 
-     
-     var rules = {
+                valid = true ;
+                if($("#nom").val() == "" ){
+                    $("#nom").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#nom").css("border-color","#48DE14");
+                }
+                if($("#prenom").val() == "" ){
+                    $("#prenom").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#prenom").css("border-color","#48DE14");
+                }
+                if($("#date").val() == "" ){
+                    $("#date").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#date").css("border-color","#48DE14");
+                }
+                if($("#adresse").val() == "" ){
+                    $("#adresse").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#adresse").css("border-color","#48DE14");
+                }
+                if($("#mail").val() == "" ){
+                    $("#mail").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#mail").css("border-color","#48DE14");
+                }
 
-        nom:{
-             required: true
-         },
-         messages: {
-            nom: {
-                required: "vous devez entrée votre nom"
-            }
-        },
-        prenom:{
-            required: true
-        },
-        messages: {
-            prenom: {
-                required: "vous devez entrée votre prénom"
-            }
-        },
-        date:{
-            required: true
-        },
-        messages: {
-            date: {
-                required: "vous devez entrée votre date de naissance"
-            }
-        },
-        adresse:{
-            required: true
-        },
-        messages: {
-            adresse: {
-                required: "vous devez entrée votre adresse"
-            }
-        },
-        mail:{
-            required: true
-        },
-        messages: {
-            mail: {
-                required: "vous devez entrée un mail valide "
-            }
-        },
-        
-        
-     }
+                return valid;
 
-     
- });
+            });
+
+
+    $("#envoyer").click(function(){
+
+         if($("#nom").val() !== "" && $("#prenom").val() !== "" && $("#date").val() !== "" 
+         && $("#adresse").val() !== "" && $("#mail").val() !== "" ){
+            
+              $("#myModal").modal("show");
+              $(".modal-title").html("Bienvenue");
+                
+        }
+
+    });
+    
+
+});
