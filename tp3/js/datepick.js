4459721ad40c62021b8bbcd9f43fbe8182c54fea
fellayoucef <youceff.fella@gmail.com>
2018-11-12 17:34:27 +0100

    TP3 FINAL

--- tp3/js/datepick.js
+++ tp3/js/datepick.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 
-var d = new Date();
-var mois = d.getMonth()+1;
-var jour = d.getDate();
-var année = d.getFullYear();
-var jourcourant = jour + '/' + mois + '/' + année;
-
-    $("#dn").datepicker({
-        dateFormat: "dd/mm/yy",
-        maxDate: "jourcourant"
-
-});
-});
+    var d = new Date();
+    var mois = d.getMonth()+1;
+    var jour = d.getDate();
+    var année = d.getFullYear();
+    var jourcourant = jour + '/' + mois + '/' + année;
+    
+        $("#dn").datepicker({
+            dateFormat: "dd/mm/yy",
+            maxDate: "jourcourant"
+    
+    });
+    });
